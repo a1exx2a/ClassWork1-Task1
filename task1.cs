8a5ef53d9003acf1bc31bc962421305416cd6f53
a1exx2a <a1exx2a@yandex.ru>
2024-12-08 17:35:22 +0300

    init commit

---
+++ task1.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int y = 0;
+        int[] x = {1,2,3,4,5,6,7,8,9,10};
+        foreach (int i in x) {
+            y = y + i;
+        }
+        Console.WriteLine(y);
+
+    }
+}
